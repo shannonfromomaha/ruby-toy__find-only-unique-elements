9302272bdeb1f4e3c3f168951568d0abf1a00bf0
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-21 04:31:19 +0000

    unique_done

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,8 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+     count_hash = arr.each_with_object (Hash.new(0)) { |element, counts| counts[element] += 1 }
+    new_hash = count_hash.select {|key,value| value == 1}
+    answer_arr = new_hash.keys
+    return answer_arr
 end
